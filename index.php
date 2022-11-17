0c371af4a64b5af0e6222e93ee0cff2cf3244f59
Marián Hriň <114649900+M4jkky@users.noreply.github.com>
2022-11-17 19:43:41 +0100

    aasdasd

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php echo '<p>First web on Azure</p>'; ?>
+<?php
+        echo '<h1>index2.php web page with parameters</h1>';
+
+        echo '<h2> https://phpipaiot.azurewebsites.net/index2.php?a=10&b=2 </h2>';
+        
+        $sn1 = $_GET["a"];
+        $sn2 = $_GET["b"];
+        
+        $text = "a=" . $sn1 . " b=" . $sn2;
+        $sum = $sn1 + $sn2;
+        
+        echo $text;
+        echo "<br>";
+        echo "Sum:" . $sum;
+    ?>
