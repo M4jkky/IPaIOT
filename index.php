307530bba1aaf9190669bee8b717edcce6935a8a
Marián Hriň <114649900+M4jkky@users.noreply.github.com>
2022-11-18 09:49:31 +0100

    formular

--- index.php
+++ index.php
@@ -1,15 +1,260 @@
-<?php
-        echo '<h1>index2.php web page with parameters</h1>';
+<html lang="sk">
 
-        echo '<h2> https://phpipaiot.azurewebsites.net/index2.php?a=10&b=2 </h2>';
-        
-        $sn1 = $_GET["a"];
-        $sn2 = $_GET["b"];
-        
-        $text = "a=" . $sn1 . " b=" . $sn2;
-        $sum = $sn1 + $sn2;
-        
-        echo $text;
-        echo "<br>";
-        echo "Sum:" . $sum;
-    ?>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <style media="screen">
+            html {
+                height: 100%;
+            }
+            body {
+                margin:0;
+                padding:0;
+                font-family: sans-serif;
+                background: linear-gradient(#141e30, #243b55);
+            }
+
+            .login-box {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                width: 400px;
+                padding: 40px;
+                transform: translate(-50%, -50%);
+                background: rgba(0,0,0,.5);
+                box-sizing: border-box;
+                box-shadow: 0 15px 25px rgba(0,0,0,.6);
+                border-radius: 10px;
+            }
+
+            .login-box h2 {
+                margin: 0 0 30px;
+                padding: 0;
+                color: #fff;
+                text-align: center;
+            }
+
+            .login-box .user-box {
+                position: relative;
+            }
+
+            .login-box .user-box input {
+                width: 100%;
+                padding: 10px 0;
+                font-size: 16px;
+                color: #fff;
+                margin-bottom: 30px;
+                border: none;
+                border-bottom: 1px solid #fff;
+                outline: none;
+                background: transparent;
+            }
+            .login-box .user-box label {
+                position: absolute;
+                top:0;
+                left: 0;
+                padding: 10px 0;
+                font-size: 16px;
+                color: #fff;
+                pointer-events: none;
+                transition: .5s;
+            }
+
+            .login-box .user-box input:focus ~ label,
+            .login-box .user-box input:valid ~ label {
+                top: -20px;
+                left: 0;
+                color: #03e9f4;
+                font-size: 12px;
+            }
+
+            .login-box form button {
+                position: relative;
+                display: inline-block;
+                padding: 10px 20px;
+                color: #03e9f4;
+                font-size: 16px;
+                text-decoration: none;
+                text-transform: uppercase;
+                overflow: hidden;
+                transition: .5s;
+                margin-top: 40px;
+                letter-spacing: 4px
+            }
+
+            .login-box button:hover {
+                background: #03e9f4;
+                color: #fff;
+                border-radius: 5px;
+                box-shadow: 0 0 5px #03e9f4,
+                0 0 25px #03e9f4,
+                0 0 50px #03e9f4,
+                0 0 100px #03e9f4;
+            }
+
+            .login-box button span {
+                position: absolute;
+                display: block;
+            }
+
+            .login-box button span:nth-child(1) {
+                top: 0;
+                left: -100%;
+                width: 100%;
+                height: 2px;
+                background: linear-gradient(90deg, transparent, #03e9f4);
+                animation: btn-anim1 1s linear infinite;
+            }
+
+            @keyframes btn-anim1 {
+                0% {
+                    left: -100%;
+                }
+                50%,100% {
+                    left: 100%;
+                }
+            }
+
+            .login-box button span:nth-child(2) {
+                top: -100%;
+                right: 0;
+                width: 2px;
+                height: 100%;
+                background: linear-gradient(180deg, transparent, #03e9f4);
+                animation: btn-anim2 1s linear infinite;
+                animation-delay: .25s
+            }
+
+            @keyframes btn-anim2 {
+                0% {
+                    top: -100%;
+                }
+                50%,100% {
+                    top: 100%;
+                }
+            }
+
+            .login-box button span:nth-child(3) {
+                bottom: 0;
+                right: -100%;
+                width: 100%;
+                height: 2px;
+                background: linear-gradient(270deg, transparent, #03e9f4);
+                animation: btn-anim3 1s linear infinite;
+                animation-delay: .5s
+            }
+
+            @keyframes btn-anim3 {
+                0% {
+                    right: -100%;
+                }
+                50%,100% {
+                    right: 100%;
+                }
+            }
+
+            .login-box button span:nth-child(4) {
+                bottom: -100%;
+                left: 0;
+                width: 2px;
+                height: 100%;
+                background: linear-gradient(360deg, transparent, #03e9f4);
+                animation: btn-anim4 1s linear infinite;
+                animation-delay: .75s
+            }
+
+            @keyframes btn-anim4 {
+                0% {
+                    bottom: -100%;
+                }
+                50%,100% {
+                    bottom: 100%;
+                }
+            }
+
+            button{
+                background: #101926;
+                border: none;
+                cursor: pointer;
+            }
+
+            .checkbox{
+                color: white;
+                cursor: pointer;
+
+            }
+
+        </style>
+
+            <title>Document</title>
+        </head>
+
+        <body>
+            <div class="login-box">
+                <h2>Form</h2>
+                <form method="post">
+                    <div class="name">
+                        <div class="user-box">
+                            <input type="text" name="first" required="">
+                            <label>First Name</label>
+                        </div>
+                        <div class="user-box">
+                            <input type="text" name="last" required="">
+                            <label id="lastname">Last Name</label>
+                        </div>
+                    </div>
+                    <div class="user-box">
+                        <input type="password" name="pw" required="">
+                        <label>Password</label>
+                    </div>
+                    <div class="user-box">
+                        <input type="email" name="email" required>
+                        <label>Email</label>
+                    </div>
+                    <div class="user-box">
+                        <input type="age" name="age" required>
+                        <label>Age</label>
+                    </div>
+                    <div class="user-box">
+                        <input type="opinion" name="op" required="">
+                        <label>Opinion</label>
+                    </div>
+
+                    <button type="submit" id="send">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Submit
+                    </button>
+
+                </form>
+            </div>
+        </body>
+</html>
+
+<?php
+
+    if(isset($_POST['first']))
+    {
+        $firstName ="-------------------\n" ."First Name: ".$_POST['first']. "\n";
+        $lastName = "Last Name: ".$_POST['last']. "\n";
+        $password = "Password: ".$_POST['pw']. "\n";
+        $email = "Email: ".$_POST['email']. "\n";
+        $age = "Age: ".$_POST['age']. "\n";
+        $opinion = "Opinion: ".$_POST['op']. "\n-------------------\n";
+
+        $fp = fopen('data.txt', 'a');
+
+        fwrite($fp, $firstName);
+        fwrite($fp, $lastName);
+        fwrite($fp, $password);
+        fwrite($fp, $email);
+        fwrite($fp, $age);
+        fwrite($fp, $opinion);
+
+        fclose($fp);
+    }
+?>
+
