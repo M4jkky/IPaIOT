2436f559212d4b619b4979730f48603575323546
Marián Hriň <114649900+M4jkky@users.noreply.github.com>
2023-01-19 18:27:31 +0100

    Update skuska_vypis.php

--- skuska_vypis.php
+++ skuska_vypis.php
@@ -1,1 +1,20 @@
+<html lang="sk">
 
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <style media="screen">
+            html {
+                height: 100%;
+            }
+            body {
+                margin:0;
+                padding:0;
+                font-family: sans-serif;
+                background: linear-gradient(#141e30, #243b55);
+            }
+         </style>
+
+            <title>Document</title>
+        </head>
