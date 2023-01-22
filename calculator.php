1449ca6edfc7fa1284e8a3d30217c73ac07b1b5f
Marián Hriň <114649900+M4jkky@users.noreply.github.com>
2023-01-22 12:20:05 +0100

    a

--- calculator.php
+++ calculator.php
@@ -1,3 +1,64 @@
+<html> 
+    <style>
+        /* Calculator Container */
+.calculator {
+    width: 300px;
+    margin: 50px auto;
+    background-color: #f5f5f5;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px #ccc;
+}
+
+/* Calculator Display */
+.calculator input[name="display"] {
+    width: 100%;
+    height: 50px;
+    font-size: 1.5rem;
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    box-shadow: 0px 0px 5px #ccc;
+    text-align: right;
+    margin-bottom: 20px;
+}
+
+/* Calculator Buttons */
+.calculator input[type="button"] {
+    width: 25%;
+    height: 40px;
+    font-size: 1.2rem;
+    margin: 5px;
+    border-radius: 5px;
+    border: none;
+    background-color: #ddd;
+    color: #333;
+    transition: all 0.2s ease-in-out;
+}
+
+.calculator input[type="button"]:hover {
+    background-color: #ccc;
+    cursor: pointer;
+}
+
+/* Calculator Operators */
+.calculator input[name="plus"],
+.calculator input[name="minus"],
+.calculator input[name="times"],
+.calculator input[name="divide"] {
+    background-color: #f44336;
+    color: #fff;
+}
+
+.calculator input[name="plus"]:hover,
+.calculator input[name="minus"]:hover,
+.calculator input[name="times"]:hover,
+.calculator input[name="divide"]:hover {
+    background-color: #e53935;
+}
+    </style>
+</html>
+
 <form method="post" action="calculator.php">
     <form method="post" action="calculator.php">
     <input type="text" name="display" value="<?php echo $display; ?>"/>
