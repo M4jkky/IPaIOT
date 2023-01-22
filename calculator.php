9acf73b01d875bc6fe181599a3e1baf2bce66244
Marián Hriň <114649900+M4jkky@users.noreply.github.com>
2023-01-22 12:06:19 +0100

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -1,37 +1,28 @@
-<form method="post">
-    <input type="text" name="num1" placeholder="Enter first number">
+<form method="post" action="calculator.php">
+    <input type="text" name="display" value="<?php echo $display; ?>"/>
     <br>
-    <input type="text" name="num2" placeholder="Enter second number">
+    <input type="button" name="one" value="1" onclick="document.forms[0].display.value+='1'"/>
+    <input type="button" name="two" value="2" onclick="document.forms[0].display.value+='2'"/>
+    <input type="button" name="three" value="3" onclick="document.forms[0].display.value+='3'"/>
+    <input type="button" name="plus" value="+" onclick="document.forms[0].display.value+='+'"/>
     <br>
-    <input type="submit" name="add" value="+">
-    <input type="submit" name="sub" value="-">
-    <input type="submit" name="mul" value="*">
-    <input type="submit" name="div" value="/">
+    <input type="button" name="four" value="4" onclick="document.forms[0].display.value+='4'"/>
+    <input type="button" name="five" value="5" onclick="document.forms[0].display.value+='5'"/>
+    <input type="button" name="six" value="6" onclick="document.forms[0].display.value+='6'"/>
+    <input type="button" name="minus" value="-" onclick="document.forms[0].display.value+='-'"/>
+    <br>
+    <input type="button" name="seven" value="7" onclick="document.forms[0].display.value+='7'"/>
+    <input type="button" name="eight" value="8" onclick="document.forms[0].display.value+='8'"/>
+    <input type="button" name="nine" value="9" onclick="document.forms[0].display.value+='9'"/>
+    <input type="button" name="times" value="*" onclick="document.forms[0].display.value+='*'"/>
+    <br>
+    <input type="button" name="clear" value="c" onclick="document.forms[0].display.value=''"/>
+    <input type="button" name="zero" value="0" onclick="document.forms[0].display.value+='0'"/>
+    <input type="submit" name="equals" value="="/>
+    <input type="button" name="divide" value="/" onclick="document.forms[0].display.value+='/'"/>
 </form>
 
 <?php
-if(isset($_POST['add'])){
-    $num1 = $_POST['num1'];
-    $num2 = $_POST['num2'];
-    $result = $num1 + $num2;
-    echo "Result: ".$result;
+if (isset($_POST['equals'])) {
+    $display = eval('return ' . $_POST['display'] . ';');
 }
-if(isset($_POST['sub'])){
-    $num1 = $_POST['num1'];
-    $num2 = $_POST['num2'];
-    $result = $num1 - $num2;
-    echo "Result: ".$result;
-}
-if(isset($_POST['mul'])){
-    $num1 = $_POST['num1'];
-    $num2 = $_POST['num2'];
-    $result = $num1 * $num2;
-    echo "Result: ".$result;
-}
-if(isset($_POST['div'])){
-    $num1 = $_POST['num1'];
-    $num2 = $_POST['num2'];
-    $result = $num1 / $num2;
-    echo "Result: ".$result;
-}
-?>
