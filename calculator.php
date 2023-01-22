3759b747ac89943b84f714fb9005cb4093f8d48c
Marián Hriň <114649900+M4jkky@users.noreply.github.com>
2023-01-22 13:27:15 +0100

    aaa

--- calculator.php
+++ calculator.php
@@ -1,3 +1,11 @@
+<?php
+$display = "";
+
+if (isset($_POST['equals'])) {
+    $display = eval('return ' . $_POST['display'] . ';');
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -63,40 +71,34 @@
     }
     </style>
 </head>
-<body>
-<form>
-    <input type="text" id="display" value="<?php echo $display; ?>" />
-    <br>
-    <input type="button" name="one" value="1" onclick="appendToDisplay(this)"/>
-    <input type="button" name="two" value="2" onclick="appendToDisplay(this)"/>
-    <input type="button" name="three" value="3" onclick="appendToDisplay(this)"/>
-    <input type="button" name="plus" value="+" onclick="appendToDisplay(this)"/>
-    <br>
-    <input type="button" name="four" value="4" onclick="appendToDisplay(this)"/>
-    <input type="button" name="five" value="5" onclick="appendToDisplay(this)"/>
-    <input type="button" name="six" value="6" onclick="appendToDisplay(this)"/>
-    <input type="button" name="minus" value="-" onclick="appendToDisplay(this)"/>
-    <br>
-    <input type="button" name="seven" value="7" onclick="appendToDisplay(this)"/>
-    <input type="button" name="eight" value="8" onclick="appendToDisplay(this)"/>
-    <input type="button" name="nine" value="9" onclick="appendToDisplay(this)"/>
-    <input type="button" name="times" value="*" onclick="appendToDisplay(this)"/>
-    <br>
-    <input type="button" name="clear" value="c" onclick="clearDisplay()"/>
-    <input type="button" name="zero" value="0" onclick="appendToDisplay(this)"/>
-    <input type="button" name="equals" value="=" onclick="calculate()"/>
-    <input type="button" name="divide" value="/" onclick="appendToDisplay(this)"/>
-</form>
-</body>
+    <body>
+        <form>
+            <div class="calculator">
+                <input type="text" id="display" value="<?php echo $display; ?>" />
+                <br>
+                <input type="button" name="one" value="1" onclick="appendToDisplay(this)"/>
+                <input type="button" name="two" value="2" onclick="appendToDisplay(this)"/>
+                <input type="button" name="three" value="3" onclick="appendToDisplay(this)"/>
+                <input type="button" name="plus" value="+" onclick="appendToDisplay(this)"/>
+                <br>
+                <input type="button" name="four" value="4" onclick="appendToDisplay(this)"/>
+                <input type="button" name="five" value="5" onclick="appendToDisplay(this)"/>
+                <input type="button" name="six" value="6" onclick="appendToDisplay(this)"/>
+                <input type="button" name="minus" value="-" onclick="appendToDisplay(this)"/>
+                <br>
+                <input type="button" name="seven" value="7" onclick="appendToDisplay(this)"/>
+                <input type="button" name="eight" value="8" onclick="appendToDisplay(this)"/>
+                <input type="button" name="nine" value="9" onclick="appendToDisplay(this)"/>
+                <input type="button" name="times" value="*" onclick="appendToDisplay(this)"/>
+                <br>
+                <input type="button" name="clear" value="c" onclick="clearDisplay()"/>
+                <input type="button" name="zero" value="0" onclick="appendToDisplay(this)"/>
+                <input type="button" name="equals" value="=" onclick="calculate()"/>
+                <input type="button" name="divide" value="/" onclick="appendToDisplay(this)"/>
+            </div>
+        </form>
+    </body>
 </html>
-
-<?php
-$display = "";
-
-if (isset($_POST['equals'])) {
-    $display = eval('return ' . $_POST['display'] . ';');
-}
-?>
 
 <script>
     function appendToDisplay(btn) {
