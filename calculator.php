b1df3ba5a0cfa51963fb41b569e4d928ad4a210a
Marián Hriň <114649900+M4jkky@users.noreply.github.com>
2023-01-24 18:10:12 +0100

    pls

--- calculator.php
+++ calculator.php
@@ -190,7 +190,7 @@ if (isset($_POST['equals'])) {
 
 
     <div class="calculator">
-    <form method="post" action="">
+    <form method="post" action="" onsubmit="return false;>
         <div class="display-wrapper">
             <input type="text" id="display" name="display" value="<?php echo $display; ?>" readonly/>
             <button id="clear" onclick="clearDisplay()">C</button>
