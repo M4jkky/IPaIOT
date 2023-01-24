2e704aa11773467352b3abb30ac9daede444690b
Marián Hriň <114649900+M4jkky@users.noreply.github.com>
2023-01-24 15:41:43 +0100

    o/

--- calculator.php
+++ calculator.php
@@ -104,7 +104,7 @@ if (isset($_POST['equals'])) {
             display: inline-block;
             align-self: center;
             text-align: center;
-            margin-left: 16px;
+            margin-left: 13px;
         }   
 
         #clear:hover {
