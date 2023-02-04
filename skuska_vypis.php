5698ea0e7726f145834b101d5145174e26f413ea
Marián Hriň <114649900+M4jkky@users.noreply.github.com>
2023-02-04 09:12:47 +0100

    Update skuska_vypis.php

--- skuska_vypis.php
+++ skuska_vypis.php
@@ -1,35 +1,3 @@
-<html lang="sk">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <style media="screen">
-            html {
-                height: 100%;
-            }
-            body {
-                margin:0;
-                padding:0;
-                font-family: sans-serif;
-            }
-
-            * {
-                text-align: center;
-            }
-
-        </style>
-
-        <title>Document</title>
-    </head>
-
-    <body>
-
-    <h1 style="color:blue;">ESP VALUES</h1>
-
-    </body>
-</html>
-
 <?php
 
     $humidity = $_GET["h"];
